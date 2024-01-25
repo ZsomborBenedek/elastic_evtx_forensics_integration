ddeeda250971439d0f0a02ceff341a094bd3ee9a
ZsomborBenedek <44943540+ZsomborBenedek@users.noreply.github.com>
2024-01-25 10:43:58 +0100

    added custom path args

--- scripts/get_eventlogs.py
+++ scripts/get_eventlogs.py
@@ -15,29 +15,28 @@ def extract_and_move(zip_path):
                 zip_info.filename = os.path.basename(
                     zip_info.filename)
                 if zip_info.filename.endswith(".evtx"):
-                    zip.extract(zip_info, os.path.join('./tmp', target_folder))
+                    zip.extract(zip_info, target_folder)
 
     return target_folder
 
 
-def send_to_es(folder, host, port, index, scheme, login, pwd):
-    source_folder = os.path.join('./tmp', folder)
-    for filename in os.listdir(source_folder):
-        file_path = os.path.join(source_folder, filename)
+def send_to_es(path, host, port, index, scheme, login, pwd):
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
 
         # Check if the path is a file (not a directory)
         if os.path.isfile(file_path) and file_path.endswith('.evtx'):
+            # print(f"{file_path} - sending to es...")
             evtx2es(file_path, host, port, index, scheme,
                     login=login, pwd=pwd, multiprocess=True)
 
 
-def cleanup(folder):
-    tmp_path = os.path.join('./tmp', folder)
-    zip_path = f"{folder}.zip"
-    new_zip_path = os.path.join('./done', zip_path)
-    shutil.rmtree(tmp_path)
-    shutil.move(zip_path, new_zip_path)
-    # print(f"deleted {tmp_path}, moved {zip_path} to {new_zip_path}")
+def cleanup(file_path, extract_path, done_path):
+    shutil.rmtree(extract_path)
+
+    done_file_path = os.path.join(done_path, os.path.basename(file_path))
+    print(f"deleted {extract_path}, moving {file_path} to {done_file_path}")
+    shutil.move(file_path, done_file_path)
 
 
 def get_secure_password():
@@ -51,11 +50,37 @@ def get_secure_password():
         return None
 
 
-def main():
+def main(input_path, done_path, password, elasticsearch, port, index, scheme, login):
+    if password is None:
+        user_password = get_secure_password()
+    else:
+        user_password = str(password)
+
+    if user_password:
+        for filename in os.listdir(input_path):
+            file_path = os.path.join(input_path, filename)
+
+            if os.path.isfile(file_path) and file_path.endswith('.zip'):
+                extract_path = extract_and_move(file_path)
+                # print(f"Temporary folder is {extract_folder}")
+
+                send_to_es(extract_path, host=elasticsearch, port=port,
+                           index=index, scheme=scheme, login=login, pwd=user_password)
+                # print("Docs sent to Elasticsearch, doing cleanup...")
+
+                cleanup(file_path, extract_path, done_path)
+                # print("Cleanup done")
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Extract a ZIP files and upload the evtx files to Elasticsearch.')
-    parser.add_argument('-u', '--url', type=str,
-                        help='IP address or url of Elastic instance', required=True)
+    parser.add_argument('-in', '--input_path', type=str,
+                        help='Path of the directory of zips', required=True)
+    parser.add_argument('-d', '--donepath', type=str,
+                        help='Path of the zip when done', required=True)
+    parser.add_argument('-e', '--elasticsearch', type=str,
+                        help='IP address or url of Elastic instance', required=False, default='127.0.0.1')
     parser.add_argument('-p', '--port', type=str,
                         help='Port to send, 9200 by default', required=False, default=9200)
     parser.add_argument('-i', '--index', type=str,
@@ -69,26 +94,5 @@ def main():
 
     args = parser.parse_args()
 
-    if args.password is None:
-        user_password = get_secure_password()
-    else:
-        user_password = str(args.password)
-
-    if user_password:
-        for filename in os.listdir():
-            file_path = os.path.join(filename)
-
-            if os.path.isfile(file_path) and file_path.endswith('.zip'):
-                target_folder = extract_and_move(file_path)
-                # print(f"Temporary folder is {target_folder}")
-
-                send_to_es(target_folder, host=args.url, port=args.port,
-                           index=args.index, scheme=args.scheme, login=args.login, pwd=user_password)
-                # print("Docs sent to Elasticsearch, doing cleanup...")
-
-                cleanup(target_folder)
-                # print("Cleanup done")
-
-
-if __name__ == "__main__":
-    main()
+    main(input_path=args.input_path, done_path=args.donepath, password=args.password,
+         elasticsearch=args.elasticsearch, port=args.port, index=args.index, scheme=args.scheme, login=args.login)
